7ab35eb4171d19cc0a0ddcf564449925b02c3daa
stockerts <seanstockert@gmail.com>
2023-11-07 19:14:47 -0800

    Corrected supported HTML version

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,6 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-html4_writer = True
 html_theme = "f5_sphinx_theme"
 html_theme_path = f5_sphinx_theme.get_html_theme_path()
 #html_theme = 'sphinx_rtd_theme'
