95a099736a6d2f0d53323a1d0784bbb50889325d
stockerts <seanstockert@gmail.com>
2023-11-07 18:54:18 -0800

    Updated static value

--- conf.py
+++ conf.py
@@ -32,7 +32,7 @@ templates_path = ['_templates']
 
 html_theme = 'f5_sphinx_theme'
 html_theme_path = f5_sphinx_theme.get_html_theme_path()
-html_static_path = ['_static']
+html_static_path = ["_static"]
 html_css_files = ['theme_overrides.css']
 
 # -- Options for EPUB output
