c8297b3cedda541acb517597c630d69418819115
stockerts <seanstockert@gmail.com>
2023-11-08 09:46:35 -0800

    Updated conf with several items

--- docs/conf.py
+++ docs/conf.py
@@ -27,10 +27,31 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
+#
 html_theme = 'press'
-#html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ['_static']
 html_css_files = ['theme_overrides.css']
+
+# If true, "Created using Sphinx" is shown in the HTML footer. The default is True.
+#
+html_show_sphinx = False
+
+# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
+#
+html_show_copyright = True
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+#
+html_logo = "docs/images/f5-logo-solid-rgb_small.png"
+
+# A shorter title for the navigation bar.  Default is the same as html_title.
+#
+html_short_title = "Home"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
