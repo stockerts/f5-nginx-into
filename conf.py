24a45d63db85e7d723726034149d8ff9f49462f2
stockerts <seanstockert@gmail.com>
2023-11-07 18:27:35 -0800

    Updated Theme

--- conf.py
+++ conf.py
@@ -29,7 +29,8 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'f5_sphinx_theme'
+html_theme_path = f5_sphinx_theme.get_html_theme_path()'
 html_static_path = ['_static']
 html_css_files = ['theme_overrides.css']
 
