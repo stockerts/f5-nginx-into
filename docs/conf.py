5fcf009f7be52d9e57d7fe5e10491c0b67a86bb8
stockerts <seanstockert@gmail.com>
2023-11-07 19:11:47 -0800

    Theme update

--- docs/conf.py
+++ docs/conf.py
@@ -28,11 +28,12 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
+html4_writer = True
 html_theme = "f5_sphinx_theme"
 html_theme_path = f5_sphinx_theme.get_html_theme_path()
 #html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_css_files = ['theme_overrides.css']
+#html_css_files = ['theme_overrides.css']
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
